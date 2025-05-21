e625171040cdd82559d53bf23cfbb6038a95b4c0
Francisco Guerra <fra.guerram@duocuc.cl>
2025-05-20 20:04:22 -0400

    nuevo MicroServicio02_api_perfumes

---
+++ MicroServicio02_api_perfumes/src/main/java/MicroServicio02/MicroServicio02/MicroServicio02Application.java
@@ -0,0 +1,13 @@
+package MicroServicio02.MicroServicio02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicio02Application.class, args);
+	}
+
+}
